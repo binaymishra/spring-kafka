5d7ddd5dcbc9bd4859a708aeace55da0129ac954
Binay Mishra <binaymishramca@gmail.com>
2021-08-16 14:40:45 +0530

    Kafka spring boot java configuration

--- src/main/java/com/bm/springkafka/Application.java
+++ src/main/java/com/bm/springkafka/Application.java
@@ -1,18 +1,12 @@
 package com.bm.springkafka;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.IntStream;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 
 /**
  * @author Binay Mishra
@@ -32,26 +26,6 @@ public class Application {
 	@KafkaListener(topics = "topic-1", groupId = "group_id")
 	public void listenGreeting(String message) {
 		System.out.println("Consume message << " + message);
-	}
-
-	@Bean
-	public ProducerFactory<String, String> producerFactory() {
-		return new DefaultKafkaProducerFactory<>(senderProps());
-	}
-
-	private Map<String, Object> senderProps() {
-		Map<String, Object> props = new HashMap<>();
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		props.put(ProducerConfig.LINGER_MS_CONFIG, 10);
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		//...
-		return props;
-	}
-
-	@Bean
-	public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
-		return new KafkaTemplate<>(producerFactory);
 	}
 
 	@Bean
